a61d4c16e51e5f58bfb2d07da0bc0dc0d154cbb2
Ido <theweathermanftw@gmail.com>
2019-11-03 00:30:46 +0200

    Updates

---
+++ precache-manifest.a43c6570974a9db34f0c9ea3aa4eb939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544db2d0238ad4badc130d543ad02ce1",
+    "url": "/Ido-Hornstein-29-10-2019/index.html"
+  },
+  {
+    "revision": "5e90c80d0289adea625c",
+    "url": "/Ido-Hornstein-29-10-2019/static/css/3.d3d09d37.chunk.css"
+  },
+  {
+    "revision": "f7ad0837da8d32253557",
+    "url": "/Ido-Hornstein-29-10-2019/static/js/2.a6d29736.chunk.js"
+  },
+  {
+    "revision": "5e90c80d0289adea625c",
+    "url": "/Ido-Hornstein-29-10-2019/static/js/3.a34b33c5.chunk.js"
+  },
+  {
+    "revision": "76cff6049ac8abd6cfeb",
+    "url": "/Ido-Hornstein-29-10-2019/static/js/4.a9ff87c3.chunk.js"
+  },
+  {
+    "revision": "93fc830e74657b55b623",
+    "url": "/Ido-Hornstein-29-10-2019/static/js/main.6dbe07d3.chunk.js"
+  },
+  {
+    "revision": "6e2c086ca036fbc65ff4",
+    "url": "/Ido-Hornstein-29-10-2019/static/js/runtime-main.d8051227.js"
+  }
+]);
